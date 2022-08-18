46ad547c2b2df7de70db7b2ab3b622403a1a5791
yashsawwashere <yashsawwashere103@gmail.com>
2022-08-18 20:31:51 +0530

    first program ongoing

---
+++ bootcoding_java/src/com/bootcoding/basic/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.bootcoding.basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+
+    }
+
+
+
+}
