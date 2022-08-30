fecb3558dffa976b72db1bce63a495d90f0f09b2
yashsawwashere <yashsawwashere103@gmail.com>
2022-08-30 20:46:22 +0530

    assignments

---
+++ bootcoding_java/src/com/bootcoding/basic/Ternary.java
@@ -0,0 +1,12 @@
+package com.bootcoding.basic;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int d = a > b ? a:b;
+        System.out.println(d);
+
+
+    }
+}
