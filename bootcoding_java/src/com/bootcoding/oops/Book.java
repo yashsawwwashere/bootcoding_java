6441613c3efd2bbf828e1379af583559cdad4330
yashsawwashere <yashsawwashere103@gmail.com>
2022-09-04 13:49:58 +0530

    Basic of Oops

---
+++ bootcoding_java/src/com/bootcoding/oops/Book.java
@@ -0,0 +1,61 @@
+package com.bootcoding.oops;
+
+public class Book {
+    String title;
+    String description;
+    String author;
+    String publisherName;
+    int pages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void buy(){
+        System.out.println("buy");
+
+
+    }
+    public void sell(){
+        System.out.println("sell");
+
+    }
+
+}
+
