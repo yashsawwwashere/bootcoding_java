ad0329a77f775b988a43c5e54e164ac4a44bf61e
yashsawwashere <yashsawwashere103@gmail.com>
2023-01-16 17:19:43 +0530

    linklist

---
+++ bootcoding_java/src/com/bootcoding/Collection/Linklist/EvenNo.java
@@ -0,0 +1,32 @@
+package com.bootcoding.Collection.Linklist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EvenNo {
+    public static void main(String[] args) {
+        List<Integer> list=new LinkedList<Integer>();
+        int[] a={2,3,4,5,6,7,8,};
+        for(int i=0;i<a.length;i++){
+        list.add(a[i]);}
+//        list.add(24);
+//        list.add(25);
+//        list.add(26);
+//        list.add(27);
+
+        for(Integer in:list){
+            if(in%2==0){
+                System.out.println("no.is even");
+
+            }
+            else{
+                System.out.println("no. is odd");
+            }
+
+        }
+
+
+
+
+    }
+}
