3fa5b4ff284704ecbae4d29b52138f6cd4884d30
yashsawwashere <yashsawwashere103@gmail.com>
2023-06-09 15:22:05 +0530

    ExamQUE

---
+++ bootcoding_java/src/com/bootcoding/basic/constructor/Publication.java
@@ -0,0 +1,34 @@
+package com.bootcoding.basic.constructor;
+
+public class Publication {
+    private String company;
+    private int since;
+    private String address;
+
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSince() {
+        return since;
+    }
+
+    public void setSince(int since) {
+        this.since = since;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
