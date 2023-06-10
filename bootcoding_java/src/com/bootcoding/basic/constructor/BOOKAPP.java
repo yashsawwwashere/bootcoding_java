2c16fc26bcb8a720e846fc4f3a9cb5b98b9cb0cc
yashsawwashere <yashsawwashere103@gmail.com>
2023-06-10 13:16:32 +0530

    ExamQUE

---
+++ bootcoding_java/src/com/bootcoding/basic/constructor/BOOKAPP.java
@@ -0,0 +1,22 @@
+package com.bootcoding.basic.constructor;
+
+public class BOOKAPP{
+    public static void main(String[] args) {
+
+
+   Author variablename= new Author () ;
+  variablename.setAge (20);
+  variablename.setName("'hoolen coover");
+
+Publication publisher = new Publication ();
+publisher.setCompany("UBD");
+publisher.setAddress ("nagpur");
+publisher.setSince(1978) ;
+
+//Book book = new Book ();
+// book.setTitle(" Hoco to srack Exam) , my book. Set publisher (publisher");
+// book.setPublisher(publisher);
+//Author[] authors = new Author[2];
+//book.setAuthor(authors);
+    }
+}
